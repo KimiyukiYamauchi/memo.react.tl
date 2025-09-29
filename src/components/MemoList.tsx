4bc3ded764ef93f247ad82a5ec9ba4b6834e83b2
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-09-29 20:37:27 +0900

    MemoList, useMemoList

--- src/components/MemoList.tsx
+++ src/components/MemoList.tsx
@@ -1,40 +1,12 @@
-import { FC } from "react";
-import styled from "styled-components";
+import { useCallback, useState } from "react";
 
-interface Props {
-  memos: string[];
-  onClickDelete: (index: number) => void;
-}
+// メモ一覧を管理するカスタムフック
+export const useMemoList = () => {
+  // メモ追加
+  const addTodo = useCallback(() => {}, []);
 
-export const MemoList: FC<Props> = (props) => {
-  const { memos, onClickDelete } = props;
+  // メモ削除
+  const deleteTodo = useCallback(() => {}, []);
 
-  return (
-    <SContainer>
-      <p>メモ一覧</p>
-      <ul>
-        {memos.map((memo, index) => (
-          <li key={memo}>
-            <SMemoWrapper>
-              <p>{memo}</p>
-              <SButton onClick={() => onClickDelete(index)}>削除</SButton>
-            </SMemoWrapper>
-          </li>
-        ))}
-      </ul>
-    </SContainer>
-  );
+  return null;
 };
-
-const SButton = styled.button`
-  margin-left: 16px;
-`;
-const SContainer = styled.div`
-  border: 1px solid #ccc;
-  padding: 16px;
-  margin: 8px;
-`;
-const SMemoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
