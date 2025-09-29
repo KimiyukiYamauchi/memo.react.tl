dd9fab185e3ce906e7dba9fb4e6e36b59c6365f5
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-09-29 21:12:12 +0900

    components/App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,35 @@
-export const App = () => {
-  return <h1>簡単メモアプリ</h1>;
+import { ChangeEvent, useState, FC } from "react";
+import styled from "styled-components";
+
+export const App: FC = () => {
+  return (
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" />
+      <SButton>追加</SButton>
+      <SContainer>
+        <ul>
+          <li>
+            <SMemoWrapper>
+              <p>買い物に行く</p>
+              <SButton>削除</SButton>
+            </SMemoWrapper>
+          </li>
+        </ul>
+      </SContainer>
+    </div>
+  );
 };
+
+const SButton = styled.button`
+  margin-left: 16px;
+`;
+const SContainer = styled.div`
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin: 8px;
+`;
+const SMemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
