e5a4f027621e96b474f7586f83b7b18c8c908b59
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-12-03 15:48:37 +0900

    App.tsxの位置をapp/componentsからapp/に移動した

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOOM from "react-dom/client";
-import { App } from "./components/App";
+import { App } from "./App";
 
 const root = ReactDOOM.createRoot(
   document.getElementById("root") as HTMLElement
