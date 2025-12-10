6a3ad92203a15e39cfd6c4093e113f41534b5a3c
itc-n25015 <n25015@std.it-college.ac.jp>
2025-12-10 12:17:23 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import { FC } from "react";
+import { FC, useState, ChangeEvent } from "react";
 import styles from "./App.module.css";
 
 export const App: FC = () => {
+  const [text, setText] = useState<string>("");
+  const [memos, setMemos] = useState<string[]>([]);
+
+  
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  
+  const onClickAdd = () => {
+    if (text.trim() === "") return;
+
+    setMemos([...memos, text]);
+    setText("");
+  };
+
+  
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
+
   return (
     <div>
       <h1>簡単メモアプリ</h1>
-      <input type="text" />
-      <button className={styles.button}>追加</button>
+
+      <input type="text" value={text} onChange={onChangeText} />
+      <button className={styles.button} onClick={onClickAdd}>
+        追加
+      </button>
+
       <div className={styles.container}>
         <p>メモ一覧</p>
         <ul>
-          <li>
-            <div className={styles.memoWrapper}>
-              <p>買い物に行く</p>
-              <button className={styles.button}>削除</button>
-            </div>
-          </li>
+          {memos.map((memo, index) => (
+            <li key={index}>
+              <div className={styles.memoWrapper}>
+                <p>{memo}</p>
+                <button
+                  className={styles.button}
+                  onClick={() => onClickDelete(index)}
+                >
+                  削除
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
   );
 };
+
